075fe121f0df72e3b877a386d0e6b3dcf77e1f77
Irene Matamala Muñoz <136511839+IreneMatamala@users.noreply.github.com>
2025-10-30 13:17:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -7,82 +7,77 @@ let cards;
 let cartas;
 
 let numeros = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
-let animales = ["Perro","Perro","Gato","Gato","Pajaro","Pajaro","Tortuga","Tortuga","Conejo","Conejo","Pez","Pez","Loro","Loro","Ratón","Ratón"]
-let ingles = ["Red","Red","Yellow","Yellow","Blue","Blue","Green","Green","Black","Black","Orange","Orange","Pink","Pink","Purple","Purple"]
-let catalan = ["Llimona","Llimona","Poma","Poma","Síndria","Síndria","Cireres","Cireres","Raïms","Raïms","Maduixas","Maduixas","Taronja","Taronja","Plàtan","Plátan"]
-
+let animales = ["Perro","Perro","Gato","Gato","Pajaro","Pajaro","Tortuga","Tortuga","Conejo","Conejo","Pez","Pez","Loro","Loro","Ratón","Ratón"];
+let ingles = ["Red","Red","Yellow","Yellow","Blue","Blue","Green","Green","Black","Black","Orange","Orange","Pink","Pink","Purple","Purple"];
+let catalan = ["Llimona","Llimona","Poma","Poma","Síndria","Síndria","Cireres","Cireres","Raïms","Raïms","Maduixes","Maduixes","Taronja","Taronja","Plàtan","Plátan"];
 
 cards = numeros;
-cards = cards.sort(()=>{return Math.random()-0.5});
+cards.sort(() => Math.random() - 0.5);
 
 function mostrar(id){
-    console.log(id)
-    console.log(cards)
-    
     cartasMostrar++;
     
-    if(cartasMostrar == 1){
+    if(cartasMostrar === 1){
         cartas1 = document.getElementById(id);
         resultado1 = cards[id];
         cartas1.innerHTML = resultado1;
-        cartas1.disabled=true;
+        cartas1.disabled = true;
 
-    }else if(cartasMostrar == 2){
+    } else if(cartasMostrar === 2){
         cartas2 = document.getElementById(id);
         resultado2 = cards[id];
         cartas2.innerHTML = resultado2;
-        cartas2.disabled=true;
+        cartas2.disabled = true;
 
-        if(resultado1 == resultado2){
-        cartasMostrar = 0;
-        } else{
-            setTimeout(()=>{
+        if(resultado1 === resultado2){
+            // Marcar cartas correctas en verde
+            cartas1.classList.add('correcta');
+            cartas2.classList.add('correcta');
+            cartasMostrar = 0;
+        } else {
+            setTimeout(() => {
                 cartas1.innerHTML = '';
                 cartas2.innerHTML = '';
                 cartas1.disabled = false;
                 cartas2.disabled = false;
                 cartasMostrar = 0;
-            },300);
+            }, 500); // un poco más de tiempo para ver la carta
         }
     }
 }
 
+// Funciones para cambiar de tipo de juego
 function toAnimales(){
     reset();
-    cards=animales;
-    cards.sort(()=>{return Math.random()-0.5});
-    
+    cards = animales;
+    cards.sort(() => Math.random() - 0.5);
 }
 
 function toNumbers(){
     reset();
-    cards=numeros;
-    cards.sort(()=>{return Math.random()-0.5});
+    cards = numeros;
+    cards.sort(() => Math.random() - 0.5);
 }
 
 function toIngles(){
     reset();
-    cards=ingles;
-    cards.sort(()=>{return Math.random()-0.5});
+    cards = ingles;
+    cards.sort(() => Math.random() - 0.5);
 }
 
 function toCatalan(){
     reset();
     cards = catalan;
-    cards.sort(()=>{return Math.random()-0.5});
+    cards.sort(() => Math.random() - 0.5);
 }
+
+// Reinicia el juego
 function reset(){
-    cards.sort(()=>{return Math.random()-0.5});
-
-    cartas=document.querySelectorAll(".carta");
-
+    cartas = document.querySelectorAll(".carta");
     cartasMostrar = 0;
-
     cartas.forEach(carta => {
         carta.innerHTML = '';
         carta.disabled = false;
-
+        carta.classList.remove('correcta'); // eliminar color verde
     });
-    
-    
 }
